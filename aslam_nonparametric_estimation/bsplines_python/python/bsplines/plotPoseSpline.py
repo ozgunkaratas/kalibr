9f484c57aac8f83b22b7aab40418ac34b2c2b533
ozgunkaratas <68688270+ozgunkaratas@users.noreply.github.com>
2021-03-11 19:37:22 +0100

    added updatet posespline
    posespline will be recorded to outputs

--- aslam_nonparametric_estimation/bsplines_python/python/bsplines/plotPoseSpline.py
+++ aslam_nonparametric_estimation/bsplines_python/python/bsplines/plotPoseSpline.py
@@ -1,4 +1,7 @@
 import numpy
+from mpl_toolkits import mplot3d
+import matplotlib.pyplot as plt
+ax =plt.axes(projection='3d')
 
 def plotPoseSpline(ax, poseSpline, dt=0.1, invert=False, linespec='b-'):
     # generate a 3d curve.
@@ -7,6 +10,18 @@ def plotPoseSpline(ax, poseSpline, dt=0.1, invert=False, linespec='b-'):
         curve = numpy.array([ poseSpline.inverseTransformation(t)[0:3,3] for t in numpy.append(numpy.arange(poseSpline.t_min(),poseSpline.t_max(),dt),poseSpline.t_max())])
     else:
         curve = numpy.array([ poseSpline.position(t) for t in numpy.append(numpy.arange(poseSpline.t_min(),poseSpline.t_max(),dt),poseSpline.t_max())])
-    ax.plot3D(curve[:,0], curve[:,1], curve[:,2],linespec)
-    #print "hello"
+    #print "plotting the 3d curves"
+    #ax.plot3D(curve[:,0], curve[:,1], curve[:,2],linespec)
+    print "printing pose splines to file txt files\n"
     #print curve
+    f = open("X-coords.txt","w")
+    f.write(str(curve[:,0]))
+    f.close()
+
+    f = open("Y-coords.txt","w")
+    f.write(str(curve[:,1]))
+    f.close()
+
+    f = open("Z-coords.txt", "w")
+    f.write(str(curve[0:,2]))
+    f.close()
